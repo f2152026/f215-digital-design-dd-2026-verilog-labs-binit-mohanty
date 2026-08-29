e82a06a62774f346d10a4c93b10d7c4fad185efc
binit-mohanty <f20250002@pilani.bits-pilani.ac.in>
2026-08-29 18:27:37 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -22,5 +22,11 @@ module FA_Gate(
   and (pc1, a,   b);
   xor (ps,  a,   b);
 
+
+ // xor (ps,  a,   b);
+ // and (pc1, a,   b);
+ //xor (sum, cin, ps); 
+  //and (pc2, cin, ps);
+ // or  (cout, pc1, pc2);
 endmodule
 
